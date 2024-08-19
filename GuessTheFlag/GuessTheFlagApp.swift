dc018f8741b2dd0764410fea471832a10f8ff74d
Pablo Barrios Ponce <barponce@gmail.com>
2024-08-19 20:59:08 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pablo Barrios Ponce on 19.08.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
